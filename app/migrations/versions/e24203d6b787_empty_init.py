58999b74bbe4be57a37d21a5c614edb5f9ebf1b6
Everlyn <gachuieve32@gmail.com>
2023-12-10 11:52:52 +0300

    completed

---
+++ app/migrations/versions/e24203d6b787_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e24203d6b787
+Revises: 
+Create Date: 2023-12-10 11:40:49.470099
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e24203d6b787'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
